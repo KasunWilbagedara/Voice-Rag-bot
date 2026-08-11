087d5425a0b3fc2213c4c654b5cfe752a138b8fb
Theekshana <thds96154@gmail.com>
2026-08-11 23:38:46 +0530

    refactor: remove unused recharts type declarations and clean up imports in DynamicChart component

--- src/components/DynamicChart.tsx
+++ src/components/DynamicChart.tsx
@@ -1,23 +1,6 @@
 'use client';
 
-import React, { useState, type ComponentType } from 'react';
-
-declare module 'recharts' {
-  export const BarChart: ComponentType<any>;
-  export const Bar: ComponentType<any>;
-  export const LineChart: ComponentType<any>;
-  export const Line: ComponentType<any>;
-  export const PieChart: ComponentType<any>;
-  export const Pie: ComponentType<any>;
-  export const Cell: ComponentType<any>;
-  export const XAxis: ComponentType<any>;
-  export const YAxis: ComponentType<any>;
-  export const CartesianGrid: ComponentType<any>;
-  export const Tooltip: ComponentType<any>;
-  export const Legend: ComponentType<any>;
-  export const ResponsiveContainer: ComponentType<any>;
-}
-
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import {
   BarChart,
